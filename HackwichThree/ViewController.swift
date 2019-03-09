419937a7bf0ad1e740b321d681a09f914a74be25
CM Student <cmstudent@uhwo-acm-c-14.local>
2019-03-08 15:05:41 -1000

    Did up to label my spring 2019 class 3/8/2019

--- HackwichThree/ViewController.swift
+++ HackwichThree/ViewController.swift
@@ -12,14 +12,30 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var firstlable: UILabel!
     
+  
+    @IBOutlet weak var mynameis: UILabel!
     //set lable text to "About"
   
+    @IBOutlet weak var Tyler: UILabel!
+    
+
+    @IBOutlet weak var mymajoris: UILabel!
+    
+    @IBOutlet weak var humanitiescrativemedia: UILabel!
+    
+    @IBOutlet weak var myspring2019classesare: UILabel!
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-self.firstlable.text="About"    }
-
+self.firstlable.text="About"
+        mynameis.text = "My name is"
+ Tyler.text = "Tyler"
+  mymajoris.text = "My major is"
+humanitiescrativemedia.text = " Humantities in Creative Media"
+myspring2019classesare.text = "My 2019 Spring Class are  "
 
 }
 
+}
